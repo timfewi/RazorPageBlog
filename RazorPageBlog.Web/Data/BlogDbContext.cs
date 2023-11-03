5c20ec5122f02652c51153a357884d2243ec2481
timwitter <kontakt@timwitter.at>
2023-11-03 11:45:31 +0100

    Add project files.

---
+++ RazorPageBlog.Web/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageBlog.Web.Models.Domain;
+
+namespace RazorPageBlog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ RazorPageBlog.Web/Models/Domain/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPageBlog.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid BlogPostId { get; set; }
+        
+    }
+}
