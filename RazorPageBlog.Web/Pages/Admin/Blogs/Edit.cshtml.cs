f229b177892b6ca09a127ea51ac4ce67dff93906
timwitter <kontakt@timwitter.at>
2023-11-05 21:03:54 +0100

    Delete Blog Method added.

--- RazorPageBlog.Web/Pages/Admin/Blogs/Edit.cshtml.cs
+++ RazorPageBlog.Web/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -9,15 +9,51 @@ namespace RazorPageBlog.Web.Pages.Admin.Blogs
     {
         private readonly BlogDbContext _blogDbContext;
 
+        [BindProperty]
         public BlogPost BlogPost { get; set; }
         public EditModel(BlogDbContext blogDbContext)
         {
-            _blogDbContext = blogDbContext;  
+            _blogDbContext = blogDbContext;
         }
         public void OnGet(Guid id)
         {
             BlogPost = _blogDbContext.BlogPosts.Find(id);
-          
+
+        }
+
+        public IActionResult OnPostEdit()
+        {
+            var existingBlogPost = _blogDbContext.BlogPosts.Find(BlogPost.Id);
+
+            if (existingBlogPost != null)
+            {
+                existingBlogPost.Heading = BlogPost.Heading;
+                existingBlogPost.PageTitle = BlogPost.PageTitle;
+                existingBlogPost.Content = BlogPost.Content;
+                existingBlogPost.ShortDescription = BlogPost.ShortDescription;
+                existingBlogPost.FeatureImageUrl = BlogPost.FeatureImageUrl;
+                existingBlogPost.UrlHandle = BlogPost.UrlHandle;
+                existingBlogPost.PublishedDate = BlogPost.PublishedDate;
+                existingBlogPost.Author = BlogPost.Author;
+                existingBlogPost.Visible = BlogPost.Visible;
+
+                _blogDbContext.SaveChanges();
+            }
+            return RedirectToPage("/admin/blogs/list");
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            var existingBlogPost = _blogDbContext.BlogPosts.Find(BlogPost.Id);
+
+            if (existingBlogPost != null)
+            {
+                _blogDbContext.BlogPosts.Remove(existingBlogPost);
+                _blogDbContext.SaveChanges();
+                return RedirectToPage("/admin/blogs/list");
+            }
+
+            return Page();
         }
     }
 }
